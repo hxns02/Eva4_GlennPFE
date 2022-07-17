ca902113355b204b0b0ef988a2812dc761abf412
hxns02 <104379396+hxns02@users.noreply.github.com>
2022-07-17 16:48:45 -0400

    mensaje de guardado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";  
-import {Route, Routes } from "react-router-dom";
-import Autenticacion from "./components/Login";
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import Sales from './components/Sales';
+import ProductsAdmin from './components/products/ProductsAdmin';
+import ProductsAdd from './components/products/ProductsAdd';
+import ProductsEdit from './components/products/ProductsEdit';
+
+import ClientsAdmin from './components/clients/ClientsAdmin';
+import ClientsAdd from './components/clients/ClientsAdd';
+import ClientsEdit from './components/clients/ClientsEdit';
+
 
 function App() {
-    return ( 
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Autenticacion/>}></Route> 
-            </Routes>   
-        </div>
-    );
-} 
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/sales" element={<Sales />}></Route>
+        <Route path="/products" element={<ProductsAdmin />}></Route>
+        <Route path="/products/add" element={<ProductsAdd />}></Route>
+        <Route path="/products/edit" element={<ProductsEdit />}></Route> 
+        <Route path="/clients/add" element={<ClientsAdd />}></Route>
+        <Route path="/clients" element={<ClientsAdmin />}></Route>
+        <Route path="/clients/edit" element={<ClientsEdit />}></Route>
+      </Routes>
+    
+    </div>
+  );
+}
 
 export default App;
